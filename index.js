ec6d079d0fa3d6b215690c999bae4686c897ba4e
Exz-03 <119024609+Exz-03@users.noreply.github.com>
2022-12-09 20:04:22 +0700

    Hhe

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(secure)
 app.use(express.static("public"))
 
 app.use('/', mainrouter);
-app.use('/docs', mainrouter);
+app.use('/api', apirouter);
 
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
